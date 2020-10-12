8e8a0a04430e1fff3f192d7e8e5081d71734d636
aptmcl <antonio.menezes.leitao@gmail.com>
2020-10-12 11:53:29 +0100

    First working version.

--- Julia/KhepriAutoCAD/src/KhepriAutoCAD.jl
+++ Julia/KhepriAutoCAD/src/KhepriAutoCAD.jl
@@ -1,5 +1,28 @@
 module KhepriAutoCAD
+using Reexport
+@reexport using KhepriBase
+using Dates
+using ColorTypes
+using Sockets
 
-# Write your package code here.
+# resolve conflicts
+using KhepriBase:
+    XYZ,
+    Text
 
+# functions that need specialization
+import Base:
+    show
+import KhepriBase:
+    backend_name,
+    #void_ref,
+    realize,
+    parse_signature,
+    encode,
+    decode,
+    backend_render_view,
+    backend_realistic_sky
+
+using EzXML
+include("AutoCAD.jl")
 end
