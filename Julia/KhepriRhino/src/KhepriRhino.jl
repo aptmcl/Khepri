399988fc2fcaa3a8ad7a0abe15618a2c6553c9d5
aptmcl <antonio.menezes.leitao@gmail.com>
2020-11-10 20:18:30 +0000

    First version, made from the old Khepri's Rhino backend.

--- Julia/KhepriRhino/src/KhepriRhino.jl
+++ Julia/KhepriRhino/src/KhepriRhino.jl
@@ -1,5 +1,33 @@
 module KhepriRhino
+using KhepriBase
+using Sockets
 
-# Write your package code here.
+# functions that need specialization
+include(khepribase_interface_file())
 
+include("Rhino.jl")
+
+function __init__()
+  set_backend_family(default_wall_family(), rhino, rhino_layer_family("Wall"))
+  set_backend_family(default_slab_family(), rhino, rhino_layer_family("Slab"))
+  set_backend_family(default_roof_family(), rhino, rhino_layer_family("Roof"))
+  set_backend_family(default_beam_family(), rhino, rhino_layer_family("Beam"))
+  set_backend_family(default_column_family(), rhino, rhino_layer_family("Column"))
+  set_backend_family(default_door_family(), rhino, rhino_layer_family("Door"))
+  set_backend_family(default_panel_family(), rhino, rhino_layer_family("Panel"))
+
+  set_backend_family(default_table_family(), rhino, rhino_layer_family("Table"))
+  set_backend_family(default_chair_family(), rhino, rhino_layer_family("Chair"))
+  set_backend_family(default_table_chair_family(), rhino, rhino_layer_family("TableChairs"))
+
+  set_backend_family(default_curtain_wall_family(), rhino, rhino_layer_family("CurtainWall"))
+  set_backend_family(default_curtain_wall_family().panel, rhino, rhino_layer_family("CurtainWall-Panel"))
+  set_backend_family(default_curtain_wall_family().boundary_frame, rhino, rhino_layer_family("CurtainWall-Boundary"))
+  set_backend_family(default_curtain_wall_family().transom_frame, rhino, rhino_layer_family("CurtainWall-Transom"))
+  set_backend_family(default_curtain_wall_family().mullion_frame, rhino, rhino_layer_family("CurtainWall-Mullion"))
+  #current_backend(rhino)
+
+  set_backend_family(default_truss_node_family(), rhino, rhino_layer_family("TrussNodes"))
+  set_backend_family(default_truss_bar_family(), rhino, rhino_layer_family("TrussBars"))
+end
 end
