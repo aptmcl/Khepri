74c3de9d0e2541fdbc7160aa7ec5ce1a584d8898
Antonio Menezes Leitao <antonio.menezes.leitao@gmail.com>
2021-07-04 21:47:54 +0100

    Added 2D & 3D coordinates. Moved in_world convertions. Added dimensions. Added transforms. Adapted to the new backend protocol.

--- Julia/KhepriTikZ/src/TikZ.jl
+++ Julia/KhepriTikZ/src/TikZ.jl
@@ -4,7 +4,7 @@ export tikz,
 
 take(out::IO) =
   let contents = String(take!(out))
-    println(out, contents)
+    print(out, contents)
     contents
  end
 
@@ -14,8 +14,11 @@ tikz_e(out::IO, arg) =
     println(out, ";")
   end
 
+export use_wireframe
+use_wireframe = Parameter(false)
+
 tikz_draw(out::IO, filled=false) =
-  print(out, filled ? "\\fill " : "\\draw ")
+  print(out, filled && ! use_wireframe() ? "\\fill " : "\\draw ")
 
 tikz_number(out::IO, x::Real) =
   isinteger(x) ?
@@ -29,8 +32,28 @@ tikz_cm(out::IO, x::Real) = begin
   print(out, "cm")
 end
 
+tikz_2d_coord(out::IO, c::Loc) =
+  let c = in_world(c)
+    print(out, "(")
+    tikz_number(out, c.x)
+    print(out, ",")
+    tikz_number(out, c.y)
+    print(out, ")")
+  end
+
+tikz_3d_coord(out::IO, c::Loc) =
+  let c = in_world(c)
+    print(out, "(")
+    tikz_number(out, c.x)
+    print(out, ",")
+    tikz_number(out, c.y)
+    print(out, ",")
+    tikz_number(out, c.z)
+    print(out, ")")
+  end
+#
 tikz_coord(out::IO, c::Loc) =
-  begin
+  let c = in_world(c)
     print(out, "(")
     tikz_number(out, c.x)
     print(out, ",")
@@ -42,8 +65,9 @@ tikz_coord(out::IO, c::Loc) =
     print(out, ")")
   end
 
+
 tikz_pgfpoint(out::IO, c::Loc) =
-  begin
+  let c = in_world(c)
     if ! iszero(c.z)
       error("Can't handle 3D coords")
     end
@@ -120,9 +144,10 @@ tikz_maybe_arc(out::IO, c::Loc, r::Real, ai::Real, da::Real, filled=false) =
     end
   end
 
-tikz_line(out::IO, pts::Locs) =
+tikz_line(out::IO, pts::Locs, options::String="") =
   begin
     tikz_draw(out, false)
+    print(out, options)
     tikz_coord(out, first(pts))
     for pt in Iterators.drop(pts, 1)
       print(out, "--")
@@ -140,6 +165,15 @@ tikz_dimension(out::IO, p::Loc, q::Loc, text::AbstractString) =
     print(out, "}{;")
     print(out, text)
     println(out, "};")
+  end
+
+tikz_dim_line(out::IO, p::Loc, q::Loc, text::AbstractString, outside) =
+  begin
+    print(out, "\\draw[fill=black,latex-latex, very thin]")
+    tikz_coord(out, p)
+    print(out, "--")
+    tikz_coord(out, q)
+    println(out, "node[midway,auto=left]{$text};")
   end
 
 tikz_closed_line(out::IO, pts::Locs, filled::Bool=false) =
@@ -214,7 +248,7 @@ tikz_rectangle(out::IO, p::Loc, w::Real, h::Real, filled::Bool=false) =
     tikz_draw(out, filled)
     tikz_coord(out, p)
     print(out, "rectangle")
-    tikz_coord(out, p+vxy(w, h))
+    tikz_coord(out, add_xy(p, w, h))
     println(out, ";")
   end
 
@@ -234,31 +268,58 @@ tikz_text(out::IO, txt, p::Loc, h::Real) =
     tikz_e(out, "}")
   end
 
-# FINISH THIS
 tikz_transform(out::IO, f::Function, c::Loc) =
-  begin
+  let m = c.cs.transform,
+      t = in_world(c),
+      a = m[1,1], b = m[2,1], c = m[1,2], d = m[2,2],
+      tx = t.x, ty = t.y
     print(out, "\\begin{scope}")
-    print(out, "[shift={")
-    tikz_coord(out, c)
-    print(out, "}]")
-    print(out, "]")
+    println(out, "[cm={$a, $b, $c, $d, ($tx, $ty)}]")
     f(out)
     tikz_e(out, "\\end{scope}")
   end
 
 tikz_set_view(out::IO, camera::Loc, target::Loc, lens::Real) =
   let v = camera - target,
-      contents = String(take!(out))
+      contents = String(take(out)),
+      out = IOBuffer()
     print(out, "\\tdplotsetmaincoords{")
     tikz_number(out, rad2deg(sph_psi(v)))
     print(out, "}{")
     tikz_number(out, rad2deg(sph_phi(v))+90)
     println(out, "}")
-    println(out, "\\begin{tikzpicture}[tdplot_main_coords,fill opacity=0.4]")
+    println(out, "\\begin{tikzpicture}[tdplot_main_coords$(use_wireframe() ? "" : ",fill=gray")]") #)opacity=0.2")]")
     print(out, contents)
     println(out, "\\end{tikzpicture}")
+    String(take!(out))
   end
 
+tikz_set_view(out::IO, camera::Loc, target::Loc, lens::Real) =
+  let v = camera - target,
+      contents = String(take(out)),
+      out = IOBuffer()
+    println(out, raw"\begin{tikzpicture}")
+    #println(out, "\\begin{axis}[view={$(rad2deg(sph_phi(v))+90)}{$(rad2deg(sph_psi(v)))},axis equal image,hide axis,colormap/blackwhite]")
+    println(out, "\\begin{axis}[axis equal image,hide axis,colormap/blackwhite]")
+    print(out, contents)
+    println(out, raw"\end{axis}")
+    println(out, raw"\end{tikzpicture}")
+    String(take!(out))
+  end
+
+tikz_set_view_top(out::IO) =
+  let contents = String(take(out)),
+      out = IOBuffer()
+    println(out, "\\begin{tikzpicture}[scale=1]")
+    print(out, contents)
+    println(out, "\\end{tikzpicture}")
+    String(take!(out))
+  end
+
+#=
+  \begin{tikzpicture}
+  \begin{axis}[view={135}{45},axis equal image,scale=4,hide axis
+=#
 #
 
 abstract type TikZKey end
@@ -275,14 +336,14 @@ const TikZ = IOBufferBackend{TikZKey, TikZId}
 
 KhepriBase.void_ref(b::TikZ) = TikZNativeRef(nothing)
 
-create_TikZ_connection() = IOBuffer()
-
-const tikz = TikZ(create_TikZ_connection())
+const tikz = TikZ(lens=0)
 
 KhepriBase.backend_name(b::TikZ) = "TikZ"
 
-tikz_output(b::TikZ=tikz) = take(connection(b))
-
+tikz_output(b::TikZ=tikz) =
+  b.lens == 0 ?
+    tikz_set_view_top(connection(b)) :
+    tikz_set_view(connection(b), b.camera, b.target, b.lens)
 withTikZXForm(f, out, c) =
   if is_world_cs(c.cs)
     f(out, c)
@@ -296,18 +357,18 @@ KhepriBase.b_delete_all_refs(b::TikZ) =
   truncate(connection(b), 0)
 
 KhepriBase.b_point(b::TikZ, p) =
-  tikz_pgfpoint(connection(b), in_world(p))
+  tikz_pgfpoint(connection(b), p)
 
 KhepriBase.b_line(b::TikZ, ps, mat) =
-  tikz_line(connection(b), map(in_world, ps))
+  tikz_line(connection(b), ps)
 
 KhepriBase.b_polygon(b::TikZ, ps, mat) =
-  tikz_closed_line(connection(b), map(in_world, s.vertices))
+  tikz_closed_line(connection(b), ps)
 
 KhepriBase.b_spline(b::TikZ, ps, v0, v1, interpolator, mat) =
   if (v0 == false) && (v1 == false)
     #TikZSpline(connection(b), s.points)
-    tikz_hobby_spline(connection(b), map(in_world, ps), false)
+    tikz_hobby_spline(connection(b), ps, false)
   elseif (v0 != false) && (v1 != false)
     TikZInterpSpline(connection(b), ps, v0, v1)
   else
@@ -318,7 +379,7 @@ KhepriBase.b_spline(b::TikZ, ps, v0, v1, interpolator, mat) =
   end
 
 KhepriBase.b_closed_spline(b::TikZ, ps, mat) =
-  tikz_hobby_closed_spline(connection(b), map(in_world, ps))
+  tikz_hobby_closed_spline(connection(b), ps)
 
 KhepriBase.b_circle(b::TikZ, c, r, mat) =
   withTikZXForm(connection(b), c) do out, cc
@@ -335,15 +396,52 @@ KhepriBase.b_rectangle(b::TikZ, c, dx, dy, mat) =
     tikz_rectangle(out, cc, dx, dy)
   end
 
+# KhepriBase.b_trig(b::TikZ, p1, p2, p3, mat) =
+#   tikz_closed_line(connection(b), [p1, p2, p3], true)
+
+#
+# KhepriBase.b_trig(b::TikZ, p1, p2, p3, mat) =
+#   let io = connection(b)
+#     println(io, raw"\addplot3[patch,table/row sep=\\,patch table={")
+#     println(io, "0 1 2 \\")
+#     println(io, raw")}] table [row sep=\\] {")
+#      x y z c\\
+#      0 1 0 0\\
+#      0 0 -1 0\\
+#      -1 0 0 0\\
+#      0 0 1 0\\
+#      1 0 0 0\\
+#     };
+#
+# KhepriBase.b_quad(b::TikZ, p1, p2, p3, p4, mat) =
+#   tikz_closed_line(connection(b), [p1, p2, p3, p4], true)
+
 KhepriBase.b_trig(b::TikZ, p1, p2, p3, mat) =
-  tikz_closed_line(connection(b), in_world.([p1, p2, p3]), true)
+  let io = connection(b)
+    print(io, raw"\addplot3[patch,shader=interp] coordinates {")
+    tikz_3d_coord(io, p1)
+    tikz_3d_coord(io, p2)
+    tikz_3d_coord(io, p3)
+    println(io, "};")
+  end
+
+KhepriBase.b_quad(b::TikZ, p1, p2, p3, p4, mat) =
+  invoke(b_quad, Tuple{Backend, Any, Any, Any, Any, Any}, b, p1, p2, p3, p4, mat)
+  # let io = connection(b)
+  #   print(io, raw"\addplot3[patch,shader=interp] coordinates {")
+  #   tikz_3d_coord(io, p1)
+  #   tikz_3d_coord(io, p2)
+  #   tikz_3d_coord(io, p3)
+  #   tikz_3d_coord(io, p4)
+  #   println(io, "};")
+  # end
 
 KhepriBase.b_surface_polygon(b::TikZ, ps, mat) =
-  tikz_closed_line(connection(b), in_world.(ps), true)
+  tikz_closed_line(connection(b), ps, true)
+  #=
 
 KhepriBase.b_surface_polygon_with_holes(b::TikZ, ps, qss, mat) =
-  tikz_closed_lines(
-    connection(b), [in_world.(ps), [in_world.(qs) for qs in qss]...], true)
+  tikz_closed_lines(connection(b), [ps, qss...], true)
 
 KhepriBase.b_surface_circle(b::TikZ, c, r, mat) =
   withTikZXForm(connection(b), c) do out, cc
@@ -354,52 +452,61 @@ KhepriBase.b_surface_arc(b::TikZ, c, r, α, Δα, mat) =
   withTikZXForm(connection(b), c) do out, cc
     tikz_maybe_arc(out, cc, r, α, Δα, true)
   end
-
-realize(b::TikZ, s::Ellipse) =
-  withTikZXForm(connection(b), s.center) do out, c
-    tikz_ellipse(out, c, s.radius_x, s.radius_y, 0, false)
-  end
-
-realize(b::TikZ, s::SurfaceEllipse) =
-  withTikZXForm(connection(b), s.center) do out, c
-    tikz_ellipse(out, c, s.radius_x, s.radius_y, 0, true)
-  end
-
-realize(b::TikZ, s::EllipticArc) =
-  error("Finish this")
+=#
+# realize(b::TikZ, s::Ellipse) =
+#   withTikZXForm(connection(b), s.center) do out, c
+#     tikz_ellipse(out, c, s.radius_x, s.radius_y, 0, false)
+#   end
+#
+# realize(b::TikZ, s::SurfaceEllipse) =
+#   withTikZXForm(connection(b), s.center) do out, c
+#     tikz_ellipse(out, c, s.radius_x, s.radius_y, 0, true)
+#   end
+#
+# realize(b::TikZ, s::EllipticArc) =
+#   error("Finish this")
 
 #realize(b::TikZ, s::SurfaceElliptic_Arc) = TikZCircle(connection(b),
 
-KhepriBase.b_surface_rectangle(b::TikZ, c, dx, dy, mat) =
-  withTikZXForm(connection(b), c) do out, cc
-    tikz_rectangle(out, cc, dx, dy, true)
+# KhepriBase.b_surface_rectangle(b::TikZ, c, dx, dy, mat) =
+#   withTikZXForm(connection(b), c) do out, cc
+#     tikz_rectangle(out, cc, dx, dy, true)
+#   end
+
+KhepriBase.b_text(b::TikZ, str, p, size, mat) =
+  #invoke(b_text, Tuple{Backend, Any, Any, Any, Any}, b, str, p, size, mat)
+  withTikZXForm(connection(b), p) do out, c
+    tikz_text(out, str, c, size)
   end
 
-realize(b::TikZ, s::Text) =
-  withTikZXForm(connection(b), s.corner) do out, c
-    tikz_text(out, s.str, c, s.height)
-  end
+KhepriBase.b_dim_line(b::TikZ, p, q, tv, str, size, outside, mat) =
+  #invoke(b_dim_line, Tuple{Backend, Any, Any, Any, Any, Any, Any, Any}, b, p, q, tv, str, size, outside, mat)
+  tikz_dim_line(connection(b), p, q, str, outside)
 
-#
-realize(b::TikZ, s::SurfaceGrid) =
-  let n = size(s.points,1),
-      m = size(s.points,2)
-    for i in 1:n
-      tikz_hobby_spline(connection(b), map(in_world, s.points[i,:]), false)
-    end
-    for j in 1:m
-      tikz_hobby_spline(connection(b), map(in_world, s.points[:,j]), false)
-    end
-  end
+KhepriBase.b_ext_line(b::TikZ, p, q, mat) =
+  tikz_line(connection(b), [p, q], "[very thin]")
 
+# realize(b::TikZ, s::SurfaceGrid) =
+#   invoke(realize, Tuple{Backend, SurfaceGrid}, b, s)
+  # let n = size(s.points,1),
+  #     m = size(s.points,2)
+  #   for i in 1:n
+  #     tikz_hobby_spline(connection(b), s.points[i,:], false)
+  #   end
+  #   for j in 1:m
+  #     tikz_hobby_spline(connection(b), s.points[:,j], false)
+  #   end
+  # end
+
+# For extra non-portable stuff
+export add_tikz
+add_tikz(str) =
+  println(connection(tikz), str)
 ###
 # Dimensioning
 
 #backend_dimension(b::TikZ, pa, pb, sep, scale, style) =
 #  tikz_dimension(connection(b), pa, pb, )
-
-KhepriBase.b_set_view(b::TikZ, camera::XYZ, target::XYZ, lens::Real, aperture::Real) =
-  tikz_set_view(connection(b), in_world(camera), in_world(target), lens)
 
 #=openpdf()
       pdfname = makepdf(latex)
@@ -412,31 +519,46 @@ KhepriBase.b_set_view(b::TikZ, camera::XYZ, target::XYZ, lens::Real, aperture::R
       pdfname
   end
 =#
-const pdflatex_folder = Parameter("C:/Users/aml/AppData/Local/Programs/MiKTeX/miktex/bin/x64/")
-pdflatex_cmd(cmd::AbstractString="pdflatex") = pdflatex_folder() * cmd
+const miktex_folder = Parameter("C:/Users/aml/AppData/Local/Programs/MiKTeX/miktex/bin/x64/")
+miktex_cmd(cmd::AbstractString="pdflatex") = miktex_folder() * cmd
+
+process_tikz(path) =
+  let contents = tikz_output(),
+      path = path_replace_suffix(path, ".tex")
+    open(path, "w") do out
+      println(out, raw"\documentclass{standalone}")
+      println(out, raw"\usepackage{tikz}")
+      println(out, raw"\usetikzlibrary{patterns}")
+      println(out, raw"\usetikzlibrary{calc,fadings,decorations.pathreplacing}")
+      println(out, raw"\usetikzlibrary{shapes,fit}")
+      #println(out, raw"\usepackage{pgfplots}")
+      #println(out, raw"\pgfplotsset{compat=1.17}")
+      println(out, raw"\usepackage{tikz-3dplot}")
+      println(out, raw"\begin{document}")
+      println(out, contents)
+      println(out, raw"\end{document}")
+    end
+    # cd(dirname(path)) do
+    #   output = read(`$(miktex_cmd()) -shell-escape -halt-on-error $(path)`, String)
+    #   occursin("Error:", output) && println(output)
+    # end
+    cd(dirname(path)) do
+      run(`$(miktex_cmd("texify")) --pdf --engine=luatex --run-viewer $(path)`, wait=true)
+      #output = read(`$(miktex_cmd("texify")) --run-viewer $(path)`, String)
+      #occursin("Error:", output) && println(output)
+    end
+  end
 
 export visualize_tikz
 visualize_tikz(name="Test") =
-  let contents = tikz_output()
-    with(render_kind_dir, "TikZ",
-         render_ext, ".tex") do
-      let path = prepare_for_saving_file(render_pathname(name))
-        open(path, "w") do out
-          println(out, raw"\documentclass{standalone}")
-          println(out, raw"\usepackage{tikz}")
-          println(out, raw"\usetikzlibrary{patterns}")
-          println(out, raw"\usetikzlibrary{calc,fadings,decorations.pathreplacing}")
-          println(out, raw"\usetikzlibrary{shapes,fit}")
-          println(out, raw"\usepackage{tikz-3dplot}")
-          println(out, raw"\begin{document}")
-          println(out, contents)
-          println(out, raw"\end{document}")
-        end
-        cd(dirname(path)) do
-          output = read(`$(pdflatex_cmd()) -shell-escape -halt-on-error $(path)`, String)
-          occursin("Error:", output) && println(output)
-        end
-      end
-      @info "Tex file: $(render_pathname(name))"
-    end
+  with(render_kind_dir, "TikZ",
+       render_ext, ".tex") do
+    process_tikz(prepare_for_saving_file(render_pathname(name)))
+    @info "Tex file: $(render_pathname(name))"
   end
+
+KhepriBase.b_render_pathname(b::TikZ, name::String) =
+  path_replace_suffix(render_pathname(name), ".pdf")
+
+KhepriBase.b_render_view(b::TikZ, path) =
+  process_tikz(path)
