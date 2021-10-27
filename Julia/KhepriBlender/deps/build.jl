e258586a87106c4a8c5ad7757e176db311c27f5d
Antonio Menezes Leitao <antonio.menezes.leitao@gmail.com>
2021-10-27 21:20:25 +0100

    Present user some information to help with installation.

---
+++ Julia/KhepriBlender/deps/build.jl
@@ -0,0 +1,5 @@
+println(raw"""
+You need to manually activate the BlenderKit plugin:
+  1. Go to Edit -> Preferences -> Add-ons and search for BlenderKit.
+  2. Activate your BlenderKit account
+""")
