4efdf9915457ca9e803c0bd212a8920823c78658
Antonio Menezes Leitao <antonio.menezes.leitao@gmail.com>
2022-02-17 18:36:49 +0000

    Added missing clay material definition.

--- Julia/KhepriPOVRay/src/KhepriPOVRay.jl
+++ Julia/KhepriPOVRay/src/KhepriPOVRay.jl
@@ -11,6 +11,7 @@ function __init__()
   set_material(povray, material_glass, povray_glass)
   set_material(povray, material_grass, povray_grass)
   set_material(povray, material_wood, povray_wood)
+  set_material(povray, material_clay, povray_material("Clay", gray=0.5))
   set_material(povray, material_concrete, povray_material("Concrete", gray=0.3))
   set_material(povray, material_plaster, povray_material("InteriorWall70", gray=0.7))
 
