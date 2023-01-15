854bbd35b7f52d70b8e9424932d8081a253c46cc
Antonio Menezes Leitao <antonio.menezes.leitao@gmail.com>
2023-01-15 11:45:09 +0000

    Added line splat handling.

--- Julia/KhepriIllustrator/src/IllustratedOps.jl
+++ Julia/KhepriIllustrator/src/IllustratedOps.jl
@@ -182,7 +182,7 @@ illustrate_rectangle(p, dx, dy, p_str, dx_str, dy_str) =
     end
 
 
-# ---------------------- multiple dishpatch/overload illustrate
+# ---------------------- multiple dispatch/overload illustrate
 # not sure if we're gonna need this
 
 illustrate(a::Any) = nothing
@@ -258,17 +258,36 @@ illustrate(f::typeof(pol), ρ, ϕ, ρ_expr, ϕ_expr) =
   illustrate(+, u0(), vpol(ρ, ϕ), :(u0()), :(vpol($ρ_expr, $ϕ_expr)))
 
 
+#=
+line(p1, p2, p3) -> illustrate(line, p1, p2, p3, :p1, :p2, :p3)
+line([p1, p2, p3]) -> illustrate(line, p1, p2, p3, :p1, :p2, :p3)
+line(ps) -> illustrate(line, ps, :ps[1], :ps[2], :ps[3])
+line([ps..., p]) -> illustrate(line, [ps...,p], :ps[1], :ps[2], :ps[3], :p)
+line([ps..., qs...]) -> illustrate(line, [ps...,qs...], :ps[1], :ps[2], :ps[??], :qs[])
+line([ps..., qs...]) -> illustrate(line, [ps...,qs...], :[ps...,qs...][1], :[ps...,qs...][2], :[ps...,qs...][3])
+=#
 
 illustrate(f::typeof(line), args...) =
   # if the first arg is an array, the second is the corresponding expression, and let's try to be clever
   if length(args) > 0 && args[1] isa Vector
     let (pts, expr) = (args[1], args[2])
       if expr isa Expr && expr.head === :vect # OK, we have the vector of expressions
-        illustrate(f, pts..., expr.args...)
+        let nsplats = count(is_splat, expr.args)
+          if nsplats == 0 # no splats
+            illustrate(f, pts..., expr.args...)
+          elseif nsplats == 1 # just one splat
+            let splat_length = length(pts) - length(expr.args) + 1
+              illustrate(f, pts..., vcat([(is_splat(e) ? [:($(e.args[1])[$i]) for i in 1:splat_length] : [e]) for e in expr.args]...)...)
+            end
+          else 
+            error("Can't handle more than one splat")
+          end
+        end
       elseif expr isa Symbol # only one expression, is it just a symbol?
         illustrate(f, pts..., [:($expr[$i]) for i in 1:length(pts)]...)
       else # something else
         #HACK: what should we do?
+        error("Can't handle line($(args...)")
       end
     end
   else
@@ -287,7 +306,7 @@ illustrate(f::typeof(circle), c, r, c_expr, r_expr) =
     label(c, textify(c_expr))
     radius_illustration(c, r, textify(r_expr))
   end
-#
+
 illustrate(f::typeof(regular_polygon), n, c, r, a, n_expr, c_expr, r_expr, a_expr) =
   illustrate(+, c, vpol(r, a), c_expr, :(vpol($r_expr, $a_expr)))
 
