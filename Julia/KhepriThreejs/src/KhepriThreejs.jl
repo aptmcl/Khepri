b801f399eb7d169cce00c16730ecd52d049b19ae
aptmcl <antonio.menezes.leitao@gmail.com>
2025-10-23 20:19:21 +0100

    Complete reimplementation in TypeScript. Supports resources, bi-directional communication between frontend and backend. Primitives optimized to improve performance. Implemented GUI operations.

--- Julia/KhepriThreejs/src/KhepriThreejs.jl
+++ Julia/KhepriThreejs/src/KhepriThreejs.jl
@@ -1,12 +1,17 @@
 module KhepriThreejs
 using KhepriBase
-#using Colors
+using Colors
 import HTTP
 using Random
 
 # functions that need specialization
 include(khepribase_interface_file())
 include("Threejs.jl")
+
+
+# We need assets, such as toilets, furniture, trees, etc.
+# These can be in different formats, but we will start with the
+# OBJ/MTL format
 
 function __init__()
   set_default_materials()
